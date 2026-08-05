95674b618efa0a11a96c9c4927b933008e278b2b
Andre Byrd <andrebyrd87@gmail.com>
2026-08-05 05:13:19 -0500

    fix: sys.path.insert for benchmark

--- benchmarks/reproduce_0001b.py
+++ benchmarks/reproduce_0001b.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import asyncio
 import time
 import os
